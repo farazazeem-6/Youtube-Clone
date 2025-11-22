57d858e18754a3f99defdbe7548f559835fd82fc
faraz-dhillon <faraz.tacticminds@gmail.com>
2025-11-23 01:43:18 +0500

    made the search bar

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -11,12 +11,12 @@ const Header = () => {
   const navigate = useNavigate();
   // const inputValue = searchRef.current.value;
 
-const handleSearchButton = () => {
-  const inputValue = searchRef.current?.value || ""; // safe access
-  if (inputValue.trim()) {
-    navigate(`/results?search_query=${encodeURIComponent(inputValue)}`);
-  }
-};
+  const handleSearchButton = () => {
+    const inputValue = searchRef.current?.value || ""; // safe access
+    if (inputValue.trim()) {
+      navigate(`/results?search_query=${encodeURIComponent(inputValue)}`);
+    }
+  };
 
   return (
     <Wrapper>
@@ -30,16 +30,28 @@ const handleSearchButton = () => {
             <img className="w-24" src={YOUTUBE_LOGO_PNG} alt="Youtube_Logo" />
           </div>
           <div className="col-span-8 flex justify-center">
-            <div className="border border-gray-200 w-[450px] flex rounded-2xl">
-              <input
-                ref={searchRef}
-                type="text"
-                placeholder="Search"
-                className="border border-white rounded-tl-2xl rounded-bl-2xl px-3 text-[14px]  w-[90%] focus:border-blue-600 focus:outline-none shadow-inner"
-              />
-              <button onClick={handleSearchButton} className="border-l border-gray-200 px-4 bg-gray-50 rounded-tr-2xl rounded-br-2xl cursor-pointer hover:bg-gray-100">
-                <i className="ri-search-line w-[10%] text-lg"></i>
-              </button>
+            <div className="flex flex-col">
+              <div className="border border-gray-200 w-[450px] flex rounded-2xl">
+                <input
+                  ref={searchRef}
+                  type="text"
+                  placeholder="Search"
+                  className="border border-white rounded-tl-2xl rounded-bl-2xl px-3 text-[14px]  w-[90%] focus:border-blue-600 focus:outline-none shadow-inner"
+                />
+                <button
+                  onClick={handleSearchButton}
+                  className="border-l border-gray-200 px-4 bg-gray-50 rounded-tr-2xl rounded-br-2xl cursor-pointer hover:bg-gray-100"
+                >
+                  <i className="ri-search-line w-[10%] text-lg"></i>
+                </button>
+              </div>
+              <div className="max-h-[400px] overflow-y-auto fixed z-50 top-[45px] border border-gray-50 w-[400px] rounded-xl bg-white px-1 shadow-lg">
+                <ul>
+                  <li className="px-2 py-2 text-[13px] font-bold rounded-lg  m-1 hover:bg-gray-100 cursor-default">
+                    <i className="ri-search-line pr-2 text-sm"></i> Iphone
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
           <div className="col-span-3 flex justify-end">
