9656f04e295fd2ec5c5063f8a97f7311ad33a8dc
faraz-dhillon <faraz.tacticminds@gmail.com>
2025-12-20 23:22:10 +0500

    some issues fixess

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { likeVideo, unlikeVideo } from "../store/slices/likedSlice";
 import { formatViews } from "../utils/constants";
